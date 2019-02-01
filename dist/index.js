6665ea6ae31b793c051bf3e4afecc4c4b767211e
Hetic <Hetic@MacBook-Pro-de-Hassane.local>
2019-02-01 21:45:55 +0100

    mobile first t

--- dist/index.js
+++ dist/index.js
@@ -198,7 +198,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49485" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64669" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
